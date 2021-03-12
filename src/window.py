f7e5b19cb33585b836170113c2e500ec4daa9366
Henry Langmack <Henry.langmack@gmail.com>
2021-03-12 08:24:40 -0500

    Bold title

--- src/window.py
+++ src/window.py
@@ -3,9 +3,12 @@ import tkinter as tk
 class Window():
     def __init__(self):
         root = tk.Tk()
-        root.geometry('400x200')
+        root.geometry('800x600')
 
-        label = tk.Label(root, text = 'ZoomSchedules')
-        label.pack(padx = 0, pady = 0)
+        title = tk.Label(root, text = 'ZoomSchedules', font = 'Helvetica 40 bold')
+        title.pack(padx = 0, pady = 0)
+
+        grid = tk.Grid()
+        
 
         root.mainloop()
