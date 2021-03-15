4a5a7b4bd5998425f6e0ff972077349ffbfaeff3
moistpotato9873 <moistpotato9873@users.noreply.github.com>
2021-03-15 13:17:48 -0400

    test for alarm

---
+++ src/background.js
@@ -0,0 +1,13 @@
+chrome.runtime.onInstalled.addListener(async () => {
+    let url = chrome.runtime.getURL("welcome.html")
+    chrome.tabs.create({url});
+    console.log('opened new tab')
+});
+
+// chrome.runtime.onMessage.addListener
+//for when a schedule is created in the popup
+var Name = "john smith"
+chrome.alarms.create(Name, {periodInMinutes:0.1, delayInMinutes: 0.001});
+chrome.alarms.onAlarm.addListener(async () => {
+    console.log('Ten seconds have passed')
+});
