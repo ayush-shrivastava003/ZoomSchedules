fb2cf5d80d857391b6cd971e44aa419014cdb36d
moistpotato9873 <moistpotato9873@users.noreply.github.com>
2021-04-19 11:43:17 -0400

    set up messaging between service worker and popup

--- src/background.js
+++ src/background.js
@@ -2,3 +2,14 @@ importScripts("/schedule.js")
 
 const t = new Time();
 t.convert('15:00');
+
+console.log("about to connect");
+chrome.tabs.connect(0, {name: "background"})
+console.log("connected for bg");
+chrome.runtime.onConnect.addListener(function(port) {
+    console.log("got a msg");
+    port.onMessage.addListener(function() {
+      port.postMessage({execTime: t.convert('15:00')});
+      console.log("amogus");
+    });
+  });
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,6 +1,18 @@
-function sayHello() {
-    console.log('hello')
-    alert('hello')
+var execTime = null;
+B = document.getElementById("test");
+B.addEventListener("click", post);
+
+console.log("about to connect for content");
+var port = chrome.runtime.connect({name: "popup"});
+console.log("connected from content");
+
+function post() {
+  port.postMessage({test: "hello from popup"});
+  console.log("posted content");
 }
-console.log(navigator.serviceWorker.controller)
-alert("hello")
+
+port.onMessage.addListener(function (msg) {
+  console.log("we got a time: " + msg.execTime);
+  execTime = msg.execTime;
+  console.log("ok this is out execution time " + execTime);
+});
